a89c380b0fa99ba9c99aa62ef6eefa0fed99af60
AnethArmenta <anethalvarezza@gmail.com>
2022-04-23 17:15:11 -0500

    1.Agregando index

---
+++ index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+const Fer = new MissionCommander("Fer")
+const Oscar = new MissionCommander("Oscar")
+console.log(carlo.name)
+console.log(Fer.name)
+console.log(Oscar.name)
